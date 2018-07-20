9536e54c48076150377ba1ecc98cd26e9fbe0994
hnOsmiumm0001 <mail.osmium0001@hainan.net>
2018-07-19 18:47:49 -0700

    more transmutations

--- src/main/java/powerlessri/anotsturdymod/items/handler/WorldTransmutation.java
+++ src/main/java/powerlessri/anotsturdymod/items/handler/WorldTransmutation.java
@@ -2,9 +2,10 @@ package powerlessri.anotsturdymod.items.handler;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -14,58 +15,38 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class WorldTransmutation {
 	
 	public static void init(FMLInitializationEvent event) {
-		registerTransmutations("inter_smeltery", Blocks.STONE, Blocks.COBBLESTONE, Blocks.GRASS);
+		registerTransmutations("plant_infusion", Blocks.DIRT, Blocks.GRASS);
 		registerTransmutations("hell_of_fires", Blocks.NETHERRACK, Blocks.SOUL_SAND);
 		registerTransmutations("particles_party", Blocks.SAND, Blocks.GLASS);
 		
-//		registerTransmutations(EnumTransmutationType.TRANSFORMAL, "igneous_reform", (state) -> state.getActualState(blockAccess, pos));
+		registerTransmutations("igneous_reform",
+		        Blocks.STONE.getDefaultState(), 
+		        Blocks.COBBLESTONE.getDefaultState(),
+		        Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE),
+		        Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE), 
+		        Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE));
 	}
 	
 	
 	
 	public static List<WorldTransmutation> transmutations = new ArrayList<WorldTransmutation>();
-	public static enum EnumTransmutationType {
-		
-	    /** Given a list of blocks, and cycle through them */
-		EXCHANGE(),
-		/** Given a single IBlockState and a condition, cycle through the metas */
-		TRANSFORMAL();
-		
-	}
-	
 	
 	public IBlockState[] members;
 	private int nextInserstion = 0;
 	
 	private String id;
-	private EnumTransmutationType type;
 	
-	/** EnumTransmutationType.EXCHANGE */
-	public WorldTransmutation(EnumTransmutationType type, String id, int length) {
-		this.type = type;
+	public WorldTransmutation(String id, int length) {
 		this.id = id;
 		this.members = new IBlockState[length];
-	}
-	/** EnumTransmutationType.TRANSFORMAL */
-	public WorldTransmutation(EnumTransmutationType type, String id, IBlockState baseState) {
-	    this.type = type;
-	    this.id = id;
 	}
 	
 	
 	public String getId() {
 		return this.id;
 	}
-	public EnumTransmutationType getType() {
-		return this.type;
-	}
-	
-	// ======== Type: EXCHANGE starts ======== //
 	
 	public int indexOf(IBlockState b) {
-	    if(this.type != EnumTransmutationType.EXCHANGE)
-	        return -1;
-	    
 		for(int i = 0; i < this.members.length; i++) {
 			if(members[i] == b) {
 				return i;
@@ -76,9 +57,6 @@ public class WorldTransmutation {
 	}
 	
 	public void addMember(IBlockState block) {
-	    if(this.type != EnumTransmutationType.EXCHANGE)
-            return;
-	    
 		// Idiot input protection
 		if(this.nextInserstion == this.members.length) {
 			return;
@@ -89,13 +67,8 @@ public class WorldTransmutation {
 	}
 	
 	public IBlockState at(int i) {
-	    if(this.type != EnumTransmutationType.EXCHANGE)
-            return null;
-	    
 		return this.members[i];
 	}
-	
-	// ======== Type: EXCHANGE ends ======== //
 	
 	
 	
@@ -107,17 +80,18 @@ public class WorldTransmutation {
 	 * @param members The blocks that the transmutation will include
 	 */
 	public static void registerTransmutations(String id, Block... members) {
-		WorldTransmutation transm = new WorldTransmutation(EnumTransmutationType.EXCHANGE, id, members.length);
-		
-		for(int i = 0; i < members.length; i++) { 
-			transm.addMember(members[i].getDefaultState());
-		}
-		
-		transmutations.add(transm);
+	    WorldTransmutation transm = new WorldTransmutation(id, members.length);
+        for(int i = 0; i < members.length; i++) { 
+            transm.addMember(members[i].getDefaultState());
+        }
+        transmutations.add(transm);
 	}
 	
-	public static void registerTransmutations(String id, IBlockState baseState, Predicate<IBlockState> cond) {
-		WorldTransmutation transm = new WorldTransmutation(EnumTransmutationType.TRANSFORMAL, id, baseState);
+	public static void registerTransmutations(String id, IBlockState... states) {
+		WorldTransmutation transm = new WorldTransmutation(id, states.length);
+		Stream.of(states).forEach((s) -> {
+		    transm.addMember(s);
+		});
 		
 		transmutations.add(transm);
 	}
