244d3deb32909ca56c2ca9225eee31e3f5b833a5
hnOsmium0001 <mail.osmium0001@hainan.net>
2018-09-10 20:03:03 -0700

    hotfix: missing client proxy

--- src/main/java/powerlessri/anotsturdymod/library/utils/Reference.java
+++ src/main/java/powerlessri/anotsturdymod/library/utils/Reference.java
@@ -25,9 +25,9 @@ public class Reference {
     public static final String NAME = "ANotSturdyMod";
     public static final String VERSION = "1.0.6";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-    public static final String COMMON_PROXY_CLASS = "powerlessri.anotsturdymod.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "powerlessri.anotsturdymod.proxy.ServerProxy";
-    public static final String CLIENT_PROXY_CLASS = "powerlessri.anotsturdymod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "powerlessri.anotsturdymod.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "powerlessri.anotsturdymod.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "powerlessri.anotsturdymod.ClientProxy";
 
     // TODO load these in .lang file
 //    public static final String ERR_NON_SUBTYPED_ITEM = "Trying register sub-typed item model with non-sub-typed item.";
